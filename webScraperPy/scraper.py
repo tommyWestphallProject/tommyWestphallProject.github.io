405e4725f8dc12408d96371683b90c2dc79ba609
architgoswami <arctgoswami@gmail.com>
2024-12-17 20:47:49 -0800

    figured out how to write json from website

--- webScraperPy/scraper.py
+++ webScraperPy/scraper.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template
+# import required modules
+import requests
+import html_to_json
+import json
 
-app = Flask(__name__)
+# get URL
+# page = requests.get("https://en.wikipedia.org/wiki/Main_Page")
+page = requests.get("https://tommywestphall.fandom.com/wiki/List_of_television_series_in_the_Tommy_Westphall_Universe")
 
-@app.route('/')
-def index():
-    fib(3)
-    return render_template('index.html',result=fib(3))
+with open("fileDump/pageStatusCode.txt", "w") as f:
+    f.write(str(page.status_code))
 
-def fib(num):
-    return num
+# with open("fileDump/pageContent.json", "rb") as f:
+#     # html_to_json.convert(page.content)
+#     json.dump(html_to_json.convert(page.content), f, indent=4)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+with open("fileDump/pageContent.json", "w", encoding="utf-8") as file:
+    json.dump(html_to_json.convert(page.content), file, indent=4)
